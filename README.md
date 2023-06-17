6bcec6d9d544a73e795cfd7f4a7a2ef3e5519c3a
Adam Finkelstein <af@princeton.edu>
2023-06-17 15:04:42 -0400

    add brief description and links

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-# hello-gatsby
+# AF Hello Gatsby
 
-AF adding this to trigger workflow.
+Basic Hello World in Gatsby.
+
+Should be live -- hosted at Github Pages -- here:
+https://adamfinkelstein.github.io/hello-gatsby/
+
+Continuous deployment workflow described in
+[this Medium post](https://medium.com/@sam-king/deploying-a-gatsby-site-to-github-pages-using-github-actions-ci-cd-be0465a8cdb8). 
+It relies on the [Gatsby Publish](https://github.com/marketplace/actions/gatsby-publish) Github Marketplace action.
--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,27 @@ const IndexPage = () => {
       <h1>
         AF Hello Gatsby
       </h1>
-      <p>AF made this page.</p>
+      <p>AF made this page using Gatsby. Here are a few notes:
+        <ul>
+        <li>
+          The project was started using the gatsby CLI (setup with "npm install -g gatsby-cli") command "gatsby new" as described&nbsp;
+          <a href="https://www.gatsbyjs.com/docs/tutorial/getting-started/part-1/">here</a>. Then cut out most of the junk.
+        </li>
+        <li>
+          The source code is hosted at Github&nbsp;
+          <a href="https://github.com/adamfinkelstein/hello-gatsby">here</a>.
+        </li>
+        <li>
+          It relies on the continuous deployment workflow described in&nbsp;
+          <a href="https://medium.com/@sam-king/deploying-a-gatsby-site-to-github-pages-using-github-actions-ci-cd-be0465a8cdb8">this Medium post</a>.
+        </li>
+        <li>
+          It is published using Github Pages as described&nbsp;
+          <a href="https://docs.github.com/en/pages/getting-started-with-github-pages/creating-a-github-pages-site">here</a> and&nbsp;
+          <a href="https://docs.github.com/en/pages/getting-started-with-github-pages/configuring-a-publishing-source-for-your-github-pages-site">here</a>.
+        </li>
+        </ul>
+      </p>
     </main>
   )
 }
